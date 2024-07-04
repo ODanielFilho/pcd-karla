56ec5c7b303908a74f50cd02ac1118b7899e1b71
Daniel Filho <danielfsf2001@gmail.com>
2024-07-04 15:34:29 -0300

    add many auth things

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,25 @@
+import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { errorHandler } from './error-handler'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.setErrorHandler(errorHandler)
+
+app.register(fastifyCors)
+
+app.register(fastifyJwt, {
+  secret: 'your_jwt_secret_here', // Substitua pelo seu segredo JWT
+})
+
 app.listen({ port: 3000 }).then(() => {
   console.log('HTTP server runnig')
 })
