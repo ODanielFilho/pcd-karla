52678c2bf26a36cdd443b3bb570a982af04ce092
Daniel Filho <danielfsf2001@gmail.com>
2024-08-12 16:28:20 -0300

    torna acessivel a rota de todos os jobs

--- src/http/routes/jobs/get-all-jobs.ts
+++ src/http/routes/jobs/get-all-jobs.ts
@@ -3,91 +3,87 @@ import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 import { prisma } from '../../../lib/prisma'
 import { getUserPermissions } from '../../../utils/get-user-permissions'
-import { auth } from '../../middlewares/auth'
 import { UnauthorizedError } from '../_errors/unauthorized-error'
 
 export async function getAllJobs(app: FastifyInstance) {
-  app
-    .withTypeProvider<ZodTypeProvider>()
-    .register(auth)
-    .get(
-      '/jobs',
-      {
-        schema: {
-          tags: ['Jobs'],
-          summary: 'Get all jobs',
-          security: [{ bearerAuth: [] }],
-          response: {
-            200: z.object({
-              jobs: z.array(
-                z.object({
-                  id: z.number().int(),
-                  title: z.string(),
-                  description: z.string(),
-                  pay: z.number(),
-                  location: z.string(),
-                  benefits: z.string(),
-                  resume: z.array(z.string()),
-                  createdAt: z.string().datetime(),
-                  updatedAt: z.string().datetime(),
-                  companyId: z.string().uuid(),
-                  company: z.object({
-                    id: z.string().uuid(),
-                    name: z.string().nullable(),
-                    email: z.string().email().nullable(),
-                    avatarUrl: z.string().url().nullable(),
-                  }),
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/jobs',
+    {
+      schema: {
+        tags: ['Jobs'],
+        summary: 'Get all jobs',
+        security: [{ bearerAuth: [] }],
+        response: {
+          200: z.object({
+            jobs: z.array(
+              z.object({
+                id: z.number().int(),
+                title: z.string(),
+                description: z.string(),
+                pay: z.number(),
+                location: z.string(),
+                benefits: z.string(),
+                resume: z.array(z.string()),
+                createdAt: z.string().datetime(),
+                updatedAt: z.string().datetime(),
+                companyId: z.string().uuid(),
+                company: z.object({
+                  id: z.string().uuid(),
+                  name: z.string().nullable(),
+                  email: z.string().email().nullable(),
+                  avatarUrl: z.string().url().nullable(),
                 }),
-              ),
-            }),
-          },
+              }),
+            ),
+          }),
         },
       },
-      async (request, reply) => {
-        const userId = await request.getCurrentUserId()
-        const userRole = await request.getUserRole()
+    },
+    async (request, reply) => {
+      const userId = await request.getCurrentUserId()
+      const userRole = await request.getUserRole()
 
-        const { cannot } = getUserPermissions(userId, userRole)
+      const { cannot } = getUserPermissions(userId, userRole)
 
-        if (cannot('get', 'Job')) {
-          throw new UnauthorizedError(
-            'You are not allowed to see jobs for this company.',
-          )
-        }
+      if (cannot('get', 'Job')) {
+        throw new UnauthorizedError(
+          'You are not allowed to see jobs for this company.',
+        )
+      }
 
-        const jobs = await prisma.job.findMany({
-          select: {
-            id: true,
-            title: true,
-            description: true,
-            benefits: true,
-            location: true,
-            pay: true,
-            resume: true,
-            createdAt: true,
-            updatedAt: true,
-            companyId: true,
-            company: {
-              select: {
-                id: true,
-                name: true,
-                email: true,
-                avatarUrl: true,
-              },
+      const jobs = await prisma.job.findMany({
+        select: {
+          id: true,
+          title: true,
+          description: true,
+          benefits: true,
+          location: true,
+          pay: true,
+          resume: true,
+          createdAt: true,
+          updatedAt: true,
+          companyId: true,
+          company: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+              avatarUrl: true,
             },
           },
-          orderBy: {
-            createdAt: 'desc',
-          },
-        })
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      })
 
-        const formattedJobs = jobs.map((job) => ({
-          ...job,
-          createdAt: job.createdAt.toISOString(),
-          updatedAt: job.updatedAt.toISOString(),
-        }))
+      const formattedJobs = jobs.map((job) => ({
+        ...job,
+        createdAt: job.createdAt.toISOString(),
+        updatedAt: job.updatedAt.toISOString(),
+      }))
 
-        return reply.send({ jobs: formattedJobs })
-      },
-    )
+      return reply.send({ jobs: formattedJobs })
+    },
+  )
 }
